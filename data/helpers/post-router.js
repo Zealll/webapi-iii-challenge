97a47ac132fac6746bae6fbf283ca28db9f55b3f
unknown <eriznis@gmail.com>
2019-03-27 20:30:54 -0400

    added POST functionality

--- data/helpers/post-router.js
+++ data/helpers/post-router.js
@@ -5,7 +5,7 @@ const router = express.Router()
 // importing our data as 'posts'
 const posts = require('./postDb.js')
 
-
+// getting all the Posts...
 router.get('/', (req, res) => {
     posts
     .get()
@@ -17,6 +17,48 @@ router.get('/', (req, res) => {
         res
         .status(500)
         .json({message: "Something went wrong with the server."})
+    })
+})
+
+// getting a post by it's ID...
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+
+    posts
+    .getById(id)
+    .then(post => {
+        if(post.length < 0) {
+            res
+            .status(404)
+            .json({message: `The post with the ID of ${id} couldn't be found.`})
+        } else {
+            res
+            .status(201)
+            .json(post)
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        res
+        .status(500)
+        .json({message: "The Post could not be retrieved from the server."})
+    })
+})
+
+// adding a Post to our array...
+router.post('/', (req, res) => {
+    const post = req.body
+
+    posts
+    .insert(post)
+    .then(newPost => {
+        res.status(201).json(newPost)
+    })
+    .catch(error => {
+        console.log(error)
+        res
+        .status(500)
+        .json({message: "There was an error while saving your Post to the database."})
     })
 })
 
